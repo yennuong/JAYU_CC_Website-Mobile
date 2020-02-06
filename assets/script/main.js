2005aeb28fb15754a38ac0712c2ce0627506ced9
Nuong Nguyen <nuongnty@forixwebdesign.com>
2020-02-06 23:32:21 +0700

    update page 레스토랑 이용안내

--- assets/script/main.js
+++ assets/script/main.js
@@ -248,6 +248,35 @@
         });
     }
 
+    if ($('.nav-links').length){
+        $('.nav-links').each(function () {
+            const navSlides = $(this).find('.nav-tabs--slider')
+            const contentSlides = $(this).find('.tab-content--slider')
+
+            contentSlides.slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: true,
+                dots: false,
+                infinite: false,
+                asNavFor: navSlides,
+                adaptiveHeight: true
+            });
+
+            navSlides.slick({
+                slidesToShow: 6,
+                slidesToScroll: 1,
+                asNavFor: contentSlides,
+                dots: false,
+                arrows: false,
+                infinite: false,
+                focusOnSelect: true,
+                adaptiveHeight: true
+            });
+        });
+    }
+
+
 
 
 })(jQuery);
